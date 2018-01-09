812d1b4616d3bb541aaeb43083877423c2f0ff5a
Dennis Zelada <denniszelada@gmail.com>
2018-01-09 13:54:07 +0100

    Initial Commit

---
+++ VacationPlanner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VacationPlanner
+//
+//  Created by Dennis Zelada on 09.01.18.
+//  Copyright © 2018 Dennis Zelada. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
